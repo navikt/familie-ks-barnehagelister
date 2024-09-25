bbbe335a2f9ede13020993094d0a00a8670d7360
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2024-09-25 10:36:48 +0200

    Legger til endepunkt for mottaBarnehagelister og status

--- src/main/kotlin/no/nav/familie/ks/barnehagelister/rest/BarnehagelisterController.kt
+++ src/main/kotlin/no/nav/familie/ks/barnehagelister/rest/BarnehagelisterController.kt
@@ -1,12 +1,17 @@
 package no.nav.familie.ks.barnehagelister.rest
 
+import no.nav.familie.ks.barnehagelister.kontrakt.Skjema
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @ProtectedWithClaims(issuer = "maskinporten", claimMap = ["scope=nav:familie/v1/kontantstotte/barnehagelister"])
 @RestController
@@ -14,6 +19,22 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/barnehagelister")
 class BarnehagelisterController {
     private val logger = LoggerFactory.getLogger(BarnehagelisterController::class.java)
+
+    @PostMapping(path = ["/"])
+    fun mottaBarnehagelister(
+        @RequestBody skjema: Skjema,
+    ): ResponseEntity<String> {
+        logger.info("Mottok skjema")
+        return ResponseEntity.accepted().body("ok")
+    }
+
+    @GetMapping(path = ["/status/{transaksjonsId}"])
+    fun status(
+        @PathVariable transaksjonsId: UUID,
+    ): ResponseEntity<String> {
+        logger.info("Mottok status")
+        return ResponseEntity.ok("ok")
+    }
 
     @GetMapping(path = ["/ping"])
     fun ping(): ResponseEntity<String> {
