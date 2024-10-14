72c09af389e5d5528a8ca796282594021849b2e8
Henning Solberg <henning.solberg@nav.no>
2024-10-14 14:16:44 +0200

    Justert api etter diskusjon på fredag + startet på dokuemntasjon av felter i api

--- src/main/kotlin/no/nav/familie/ks/barnehagelister/kontrakt/Barnehagelistekontrakt.kt
+++ src/main/kotlin/no/nav/familie/ks/barnehagelister/kontrakt/Barnehagelistekontrakt.kt
@@ -1,13 +1,14 @@
 package no.nav.familie.ks.barnehagelister.kontrakt
 
 import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.Pattern
 import java.time.LocalDate
 import java.time.YearMonth
 import java.util.UUID
 
 data class SkjemaV1(
     val id: UUID,
-    val barnehager: List<Barnehage>,
+    val barnehager: List<Barnehage>?,
     val listeopplysninger: Listeopplysninger,
 )
 
@@ -29,7 +30,7 @@ data class BarnInfolinje(
     val startdato: LocalDate,
     val sluttdato: LocalDate?,
     val barn: Person,
-    val foreldre: List<Person>,
+    val foresatte: List<Person>,
     val endringstype: Endringstype,
 )
 
@@ -48,8 +49,19 @@ data class Person(
 )
 
 data class Adresse(
+    @Schema(
+        description = """Bruksenhetsnummer identifiserer en boligenhet innenfor et 
+                         bygg eller en bygningsdel. Bokstaven H, L, U eller K etterfult av 4 siffer""",
+        example = "H0101",
+    )
+    @Pattern(regexp = "(?i)^[HULK][0-9]{4}$|^$", message = "H, L, U eller K etterfult av 4 siffer")
+    val bruksenhetsnummer: String?,
     val adresselinje1: String?,
     val adresselinje2: String?,
+    @Schema(
+        description = "Norsk postnummer, fire siffer",
+        example = "0102",
+    )
     val postnummer: String,
     val poststed: String,
 )
