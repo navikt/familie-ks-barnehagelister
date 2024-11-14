5e6c29687e129ca2020334ff983a5df3841c75d6
Stig Helge Strøm <stig.helge.strom@nav.no>
2024-11-14 14:26:58 +0100

    Manuell trigge observabilit

--- src/main/kotlin/no/nav/familie/ks/barnehagelister/rest/DefaultBarnehagelisterController.kt
+++ src/main/kotlin/no/nav/familie/ks/barnehagelister/rest/DefaultBarnehagelisterController.kt
@@ -1,5 +1,7 @@
 package no.nav.familie.ks.barnehagelister.rest
 
+import io.micrometer.observation.Observation
+import io.micrometer.observation.ObservationRegistry
 import no.nav.familie.ks.barnehagelister.kontrakt.SkjemaV1
 import org.springframework.context.annotation.Profile
 import org.springframework.http.ResponseEntity
@@ -11,13 +13,37 @@ import java.util.UUID
 @RestController
 class DefaultBarnehagelisterController(
     private val barnehagelisteService: BarnehagelisteService,
+    private val observationRegistry: ObservationRegistry,
 ) : BarnehagelisterController {
     override fun mottaBarnehagelister(
         skjemaV1: SkjemaV1,
         bindingResult: BindingResult,
-    ): ResponseEntity<BarnehagelisteResponse> = barnehagelisteService.mottaBarnehagelister(skjemaV1, bindingResult)
+    ): ResponseEntity<BarnehagelisteResponse> =
+        Observation
+            .createNotStarted("service.mottaBarnehagelister", observationRegistry)
+            .observeNotNull {
+                barnehagelisteService.mottaBarnehagelister(skjemaV1, bindingResult)
+            }
 
     override fun status(transaksjonsId: UUID): ResponseEntity<BarnehagelisteResponse> = barnehagelisteService.status(transaksjonsId)
 
     override fun ping(): String = barnehagelisteService.ping()
+
+    fun <T : Any> Observation.observeNotNull(block: () -> T): T = observeBlock(block)
+
+    fun <T : Any?> Observation.observeNullable(block: () -> T): T = observeBlock(block)
+
+    private fun <T> Observation.observeBlock(block: () -> T): T {
+        start()
+        return try {
+            openScope().use {
+                block()
+            }
+        } catch (error: Throwable) {
+            error(error)
+            throw error
+        } finally {
+            stop()
+        }
+    }
 }
