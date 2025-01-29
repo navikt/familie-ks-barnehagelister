0fca7e487937ed8889f602a75b69bfd9b19b5b00
Stig Helge Strøm <stig.helge.strom@nav.no>
2025-01-29 12:21:59 +0100

    Oppdatert dokumentasjon

--- src/main/kotlin/no/nav/familie/ks/barnehagelister/config/SwaggerDocumentationConfig.kt
+++ src/main/kotlin/no/nav/familie/ks/barnehagelister/config/SwaggerDocumentationConfig.kt
@@ -42,6 +42,30 @@ class SwaggerDocumentationConfig {
                         - An endpoint is available for submitting kindergarten lists, supporting multiple kindergartens per request. Each submission must include a unique identifier.  
                         - A separate endpoint is provided for retrieving the submission status. This endpoint will indicate any validation errors that require attention.  
 
+                        ### Validation errors
+                        The API will return a 400 Bad Request if the JSON is invalid. The response is based on RFC7807 Problem Details for HTTP APIs. 
+                        
+                        If a required key in the json is missing, the response will be a 400 Bad Request with a JSON body containing the error in the detail field:  
+                        E.g. ```"detail": "Couldn't parse request due to missing or null parameter: name"```
+                        
+                        If the key value is invalid the detail of the error will be attached to the response:
+                        | Error message | Description | Example |
+                        | --- | --- | --- |
+                        | must not be blank | A key in the JSON is blank | ``` {"parameter": "kindergartens[0].navn", "detail": "must not be blank"}``` |
+                        | size must be between X and Y | A key in the JSON is blank | ``` {"parameter": "kindergartens[0].navn", "detail": "size must be between 1 and 200"}``` |
+                        | Not a valid organization number 123456789 | The organization number is not a valid organization number | ``` {"parameter": "kindergartens[0].organizationNumber", "detail": "Not a valid organization number 310028141"}``` |
+                        | Social Security Number is not valid | The organization number is not a valid organization number | ``` {"parameter": "kindergartens[0].childrenInformation[0].child.socialSecurityNumber", "detail": "Social Security Number is not valid"}``` |
+                        | H, L, U, or K followed by 4 digits | The unit number for an address is invalid | ``` {"parameter": "kindergartens[0].address.unitNumber", "detail": "H, L, U, or K followed by 4 digits"}``` |
+                        | Zip code must have 4 digits | The zip code in the address is not a valid Norwegian zip code | ``` {"parameter": "kindergartens[0].address.zipCode", "detail": "Zip code must have 4 digits"}``` |
+                        | A confidential address may not have any address fields set | The JSON has a confidential address set to true, but also  has the address | ``` {"parameter": "kindergartens[0].address.addressOrConfidentialAddress", "detail": "A confidential address may not have any address fields set"}``` |
+                        | Mandatory fields zipCode and/or postalTown are not set | Zip Code and/or postalTown are mandatory for confidential address set to false | ``` {"parameter": "kindergartens[0].childrenInformation[0].child.address.mandatoryFieldsSet", "detail": "Mandatory fields zipCode and/or postalTown are not set"}``` |
+                        
+                        ### Warnings in the status response
+                        The API will return a 200 OK if the JSON is valid, but there might be other issues with the data. So the status endpoint might return a JSON body with a list of warnings.
+                        | Warning type | Description |
+                        | --- | --- | 
+                        | OVERLAPPING_PERIOD_WITHIN_SAME_LIST | There are overlapping periods for a child inside the input request |
+                        
                         """.trimIndent(),
                     ),
             ).components(Components().addSecuritySchemes(bearer, bearerTokenSecurityScheme()))
