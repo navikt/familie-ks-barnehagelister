bdfcf9f6f24f40cd7e3b199c38d67d317842189c
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2025-01-30 13:24:47 +0100

    fjerner enum i db

--- src/main/resources/db/migration/V9__enum_type.sql
+++ src/main/resources/db/migration/V9__enum_type.sql
@@ -1,7 +1,1 @@
-CREATE TYPE etterprosesseringfeil_type AS ENUM ('OVERLAPPING_PERIOD_WITHIN_SAME_LIST');
-
-ALTER TABLE barnehageliste_valideringsfeil
-    ADD COLUMN etterprosesseringfeiltype etterprosesseringfeil_type NOT NULL DEFAULT 'OVERLAPPING_PERIOD_WITHIN_SAME_LIST';
-
-ALTER TABLE barnehageliste_valideringsfeil
-    DROP COLUMN type;
+ALTER TABLE barnehageliste_valideringsfeil RENAME type TO etterprosesseringfeiltype ;
